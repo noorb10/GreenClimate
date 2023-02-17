fac54246a46269c675847abc781bbfaf643a25c1
Noor BB <noor.bilal12@gmail.com>
2023-02-17 08:17:44 +0300

    Set restrictions

--- members/decorators.py
+++ members/decorators.py
@@ -3,7 +3,27 @@ from django.shortcuts import redirect
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home') #if user is authenticated redirect to home.
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper_func #if user isn't authenticated redirect to login or register page.
 
-        return view_func(request, *args, **kwargs)
-        
-    return wrapper_func
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                html = "<h1> You are not authorized to view this page!</h1> <br> <button type=\"button\" name=\"home\"><a href=\"/home.html\">Back to the Home Page</a></button>"
+                response = HttpResponse(html, content_type="text/html")
+                return(response)
+
+                
+        return wrapper_func
+    return decorator
--- members/views.py
+++ members/views.py
@@ -4,20 +4,27 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.models import Group 
 
 from .forms import CreateUserForm
+from .decorators import unauthenticated_user, allowed_users
 from django.contrib import messages
 
+@unauthenticated_user
 def signup_user(request):
 	form = CreateUserForm()
 	if request.method == "POST":
 		form = CreateUserForm(request.POST)
 		if form.is_valid():
-			form.save()
+			user = form.save()
 			username = form.cleaned_data['username']
 			password = form.cleaned_data['password1']
+
 			user = authenticate(username=username, password=password)
 			login(request, user)
+
+			group = Group.objects.get(name='client') #every new account is set to client group
+			user.groups.add(group)
 			user_msg = form.cleaned_data.get('username')
 			messages.success(request, "Registration Successful! Account was created for " + user_msg + ".")
 			return redirect('home')
@@ -25,7 +32,7 @@ def signup_user(request):
 		form = CreateUserForm()
 	return render(request, 'authenticate/signup.html', {'form':form, })
 
-
+@unauthenticated_user
 def login_user(request):
 	if request.method == "POST":
 		username = request.POST['username']
@@ -45,14 +52,15 @@ def logout_user(request):
 	messages.success(request, ("You were logged out!"))
 	return redirect('home')
 
-@login_required(login_url='login')
+@login_required(login_url='login') #Restricts access, unless authenticated! 
+@allowed_users(allowed_roles=['client']) #Allows access based on groups.
 def user_page(request):
 	return render(request, 'authenticate/user.html', {})
 
-@login_required(login_url='login')
+@login_required(login_url='login') #Restrict access, unless authenticated! 
 def community(request):
 	return render(request, 'authenticate/community.html', {})
 
-@login_required(login_url='login')
+@login_required(login_url='login') #Restrict access, unless authenticated! 
 def settings(request):
 	return render(request, 'authenticate/settings.html', {})
